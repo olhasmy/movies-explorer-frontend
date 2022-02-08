12594abdeb54bb250e918edb398460304bf610bf
Olga Smirnova <9441797@gmail.com>
2022-02-09 00:18:54 +0300

    pr route

--- src/ProtectedRoute/ProtectedRoute.js
+++ src/ProtectedRoute/ProtectedRoute.js
@@ -1,12 +1,11 @@
 import React from "react";
-import {Route, Redirect} from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 
 const ProtectedRoute = ({ component: Component, ...props }) => {
-
     return (
         <Route>
             {() =>
-                props.loggedIn ? <Component {...props} /> : <Redirect to="/movies"/>
+                props.loggedIn ? <Component {...props} /> : <Redirect to="./" />
             }
         </Route>
     );
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -82,6 +82,7 @@ function App() {
         setMovies([]);
         setSavedMovies([]);
         setCurrentUser({});
+        setSearchValue({});
         localStorage.clear();
         history.push("/");
     }
